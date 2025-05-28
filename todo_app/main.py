8b0e4b5999b0baf525e90e1bde275a568c0570a0
straylight <jdcodes85@gmail.com>
2025-05-28 09:45:52 -0500

    Changed read/write to with/as

--- todo_app/main.py
+++ todo_app/main.py
@@ -7,21 +7,25 @@ while True:
     match userAction:
         case 'add' | 'a':
             todo = input("Enter a todo: ") + "\n"
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.append(todo)
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show' | 's':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             for index, item in enumerate(todos):
                 item = item.strip("\n")
                 row = f"{index + 1}. {item}"
                 print(row)
+                
         case 'edit' | 'e':
             number = int(input("Which task do you want to edit? "))
             number = number - 1
